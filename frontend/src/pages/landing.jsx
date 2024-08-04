1e270eb62e98103c616d7403695183ecadf65291
anjdulam <anjalidulam03@gmail.com>
2024-08-05 03:18:50 +0530

    second

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -10,7 +10,7 @@ export default function LandingPage() {
         <div className='landingPageContainer'>
             <nav>
                 <div className='navHeader'>
-                    <h2>Apna Video Call</h2>
+                    <h2>Convo Connect App</h2>
                 </div>
                 <div className='navlist'>
                     <p onClick={() => {
@@ -32,9 +32,9 @@ export default function LandingPage() {
 
             <div className="landingMainContainer">
                 <div>
-                    <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
+                    <h1><span style={{ color: "#FF9839" }}>Convo Connect</span> </h1>
 
-                    <p>Cover a distance by Apna Video Call</p>
+                    <p>Bridging Distances, Enriching Conversations</p>
                     <div role='button'>
                         <Link to={"/auth"}>Get Started</Link>
                     </div>
